64a0ff8c8f7d42134e43e53a8feddeeda45e9126
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-09-28 12:24:44 +0300

    Csv parsed data

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const parse = require('csv-parse');
+const {parse} = require('csv-parse');
 const fs = require('fs');
 
-const results =  [];
+const results = [];
 
 fs.createReadStream('kepler_data.csv')
-    .on('data',(data) => {
-        results.push(data);
-    })
-    .on('error' , (err) => {
-        console.log(err);
-    })
-    .on('end' , () => {
-        console.log(results);
-        console.log('done');
-    });
-//parse();
-
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+  }))
+  .on('data', (data) => {
+    results.push(data);
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(results);
+    console.log('done');
+  });
+  
